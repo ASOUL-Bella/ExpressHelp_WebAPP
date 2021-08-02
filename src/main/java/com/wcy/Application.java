96a9f4223e1c95a4703e2029e6212a98dc6fda9a
一个魂 <2460033107@qq.com>
2021-08-02 14:37:03 +0800

    1

--- src/main/java/com/wcy/Application.java
+++ src/main/java/com/wcy/Application.java
@@ -4,10 +4,12 @@ package com.wcy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.wcy.mapper")
+@EnableSwagger2
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
